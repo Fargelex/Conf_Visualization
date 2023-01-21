405051b7473d2691cd6a32c5388d8c027931f7b3
Fargelex <enclave3005@hotmail.com>
2023-01-21 18:29:55 +0500

    проверка binding

---
+++ conf_visualization/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using conf_visualization.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conf_visualization.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+        private string _Title;
+
+        /// <summary>Заголовок окна</summary>
+
+        public string Title
+        {
+            get => _Title; // { return _Title; }  
+            set => Set(ref _Title, value);
+            //set 
+            //{ 
+            //   // _Title = value;
+            //    Set(ref _Title, value);
+            //}
+        } 
+        #endregion
+    }
+}
