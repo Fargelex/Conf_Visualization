6b1ca5968303e86a211f7d647bd24c003c2e0d6f
Fargelex <enclave3005@hotmail.com>
2023-01-23 14:29:37 +0500

    базовый класс команды

---
+++ conf_visualization/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conf_visualization.Infrastructure.Commands.Base
+{
+    internal class Command
+    {
+    }
+}
