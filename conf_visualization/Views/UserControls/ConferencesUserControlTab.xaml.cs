0035a35edad4bf6ecf2c752cb9846397b3c8a9ae
Fargelex <enclave3005@hotmail.com>
2023-02-06 18:42:58 +0500

    события интерфейса

--- conf_visualization/Views/UserControls/ConferencesUserControlTab.xaml.cs
+++ conf_visualization/Views/UserControls/ConferencesUserControlTab.xaml.cs
@@ -21,10 +21,55 @@ namespace conf_visualization.Views.UserControls
     /// </summary>
     public partial class ConferencesUserControlTab : UserControl
     {
+        bool GridEdited = false;
         public ConferencesUserControlTab()
         {
             InitializeComponent();
         }
 
+        private void ConferencesDataGrid_AddingNewItem(object sender, AddingNewItemEventArgs e)
+        {
+            GridEdited = true;
+            ConferencesSettingsButtonsStackPanel.Visibility = Visibility.Visible;
+        }
+        private void ConferencesDataGrid_BeginningEdit(object sender, DataGridBeginningEditEventArgs e)
+        {
+            GridEdited = true;
+            ConferencesSettingsButtonsStackPanel.Visibility = Visibility.Visible;
+        }
+
+        private void saveEditConferencesSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            ConferencesSettingsButtonsStackPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void cancelEditConferencesSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            ConferencesSettingsButtonsStackPanel.Visibility = Visibility.Collapsed;
+        }
+        
+
+        private void ConferencesDataGrid_LostFocus(object sender, RoutedEventArgs e)
+        {
+                      
+        }
+
+        private void ConferencesDataGrid_ManipulationCompleted(object sender, ManipulationCompletedEventArgs e)
+        {
+            MessageBox.Show("В таблице селекторов были изменения", "ConferencesDataGrid_ManipulationCompleted");
+        }
+
+        private void ConferencesDataGrid_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+           
+        }
+
+        private void ConferencesDataGrid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (GridEdited)
+            {
+                MessageBox.Show("В таблице селекторов были изменения", "Сохранить изменения?");
+            }
+        }
     }
 }
